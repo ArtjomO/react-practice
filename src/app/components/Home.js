fb1dd1b8b77d35a0f10540a5d8fd84dfab47208f
Artjom Ostapenko <artjom.ostapenko@gmail.com>
2018-02-11 06:47:29 +0200

    lesson 8

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -3,23 +3,29 @@ import PropTypes from "prop-types";
 import shortid from "shortid";
 
  class Home extends React.Component {
-    render(){
+     constructor(props){
+        super();
+        this.age = props.age;
+     };
+     
+     state = {
+         age: this.age
+     }
 
-        constructor(props){
-            super();
-            this.age = props.age;
-        };
-
-        onMakeOlder() {
-            this.age += 3;
-        }
-
+     onMakeOlder() {
+        console.log(this.age)
+        this.setState({
+            age: this.age + this.state.age
+        })
+    }
+     
+     render(){
         return (
             <div>
                 <p>In a new component</p>
-                <p>Your name is {this.props.name}, and you age is {this.age}</p>
+                <p>Your name is {this.props.name}, and you age is {this.state.age}</p>
                 <hr/>
-                <button className="button-primary" >Make me older!</button>
+                <button className="button-primary" onClick={this.onMakeOlder.bind(this)}>Make me older!</button>
             </div>
 
         )
